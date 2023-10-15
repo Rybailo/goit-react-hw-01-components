a6efc4738f47d277abae1cf5e5505780889bf74c
Rybailo <rybailo.orest4@gmail.com>
2023-10-15 15:03:14 +0300

    P1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,36 @@
+import './Profile.css';
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div>
+      <div className="profile">
+        <div className="description">
+          <img src={avatar} alt="User avatar" className="avatar" />
+          <p className="name">{username}</p>
+          <p className="tag">@{tag}</p>
+          <p className="location">{location}</p>
+        </div>
+
+        <ul className="stats">
+          <li>
+            <div className="statsItem">
+              <span className="label">Followers</span>
+              <span className="quantity">{stats.followers}</span>
+            </div>
+          </li>
+          <li>
+            <div className="statsItem">
+              <span className="label">Views</span>
+              <span className="quantity">{stats.views}</span>
+            </div>
+          </li>
+          <li>
+            <div className="statsItem">
+              <span className="label">Likes</span>
+              <span className="quantity">{stats.likes}</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
